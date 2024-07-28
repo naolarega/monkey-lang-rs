e394111ee9266c526493a222c6e7e67d1881542f
naol arega <naol.dev@gmail.com>
2024-07-28 20:00:16 +0300

    chore: impl ToString for token

--- crates/monkey_lang/src/token.rs
+++ crates/monkey_lang/src/token.rs
@@ -1,6 +1,60 @@
-pub type TokenType = String;
+pub enum Token {
+    ILLEGAL,
+    EOF,
 
-pub struct Token {
-    token_type: TokenType,
-    literal: String
+    // Identifiers + literals
+    IDENT { literal: String },
+    INT { literal: String },
+
+    // Operators
+    ASSIGN,
+    PLUS,
+
+    // Delimiters
+    COMMA,
+    SEMICOLON,
+
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+
+    // KeyWords
+    FUNCTION,
+    LET,
+}
+
+impl ToString for Token {
+    fn to_string(&self) -> String {
+        use Token::*;
+
+        match self {
+            IDENT { literal } => format!("IDENT {{ {literal} }}"),
+            INT { literal } => format!("INT {{ {literal} }}"),
+            others => match others {
+                ILLEGAL => "ILLEGAL",
+                EOF => "EOF",
+                ASSIGN => "=",
+                PLUS => "+",
+                COMMA => ",",
+                SEMICOLON => ";",
+                LPAREN => "(",
+                RPAREN => ")",
+                LBRACE => "{",
+                RBRACE => "}",
+                FUNCTION => "FUNCTION",
+                LET => "LET",
+                INT { literal: _ } | IDENT { literal: _ } => panic!("never reach here"),
+            }
+            .to_string(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_next_token() {
+        let input = "=+(){},;";
+    }
 }
